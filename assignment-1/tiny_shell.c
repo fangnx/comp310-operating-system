447d7e241654dbbe4b5ec38470e4e9948043c215
fangnx <fangnaxin@gmail.com>
2019-09-29 18:42:12 -0400

    Added tut examples

--- assignment-1/tiny_shell.c
+++ assignment-1/tiny_shell.c
@@ -4,13 +4,15 @@
  * @author Naxin Fang
  * @github https://github.com/fangnx
  * @created 2019-09-21 15:42:16
- * @last-modified 2019-09-27 23:42:26
+ * @last-modified 2019-09-29 18:36:49
  */
 
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/resource.h>
+#include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
@@ -34,7 +36,7 @@ int HIST_COUNT = 0;
 int get_a_line(char line[]) { return read(0, line, MAX_LINE_LENGTH); }
 
 /**
- * Parse the raw input line to shell commands.
+ * Tokenize the raw input, delimited by space.
  */
 void parse_commands(char line[], int length, char *commands[]) {
   const char delim[] = " \t\n";
@@ -47,16 +49,37 @@ void parse_commands(char line[], int length, char *commands[]) {
   token = strtok(line, delim);
   i = 0;
   while (token != NULL) {
+    // printf("%s\n", token);
+    // fflush(stdout);
     commands[i] = token;
     token = strtok(NULL, delim);
     i++;
   }
 }
 
+int find_pipe(char *args[]) {
+  int i;
+  i = 0;
+  while (args[i] != NULL) {
+    // printf("%s\n", args[i]);
+    // fflush(stdout);
+    if (strcmp(args[i], "|") == 0) {
+      return i;
+    }
+    i++;
+  }
+  return -1;
+}
+
 /**
  *
  */
 void my_system(char line[], char *args[]) {
+  char *fifo_path =
+      "/Users/fnx/Dropbox/Academics/COMP "
+      "310/comp310-operating-system/assignment-1/fifo";
+  mkfifo(fifo_path, 0777);
+
   // Update history.
   strcpy(HISTORY[HIST_COUNT % MAX_HISTORY_AMOUNT].line, line);
   HIST_COUNT++;
@@ -88,23 +111,60 @@ void my_system(char line[], char *args[]) {
     printf("New limits have been set:\nsoft limit: %llu\nhard limit: %llu\n",
            new_lim.rlim_cur, new_lim.rlim_max);
   }
-
+  //
   else if (strcmp(args[0], "exit") == 0) {
     exit(0);
-  } else {
-    pid_t pid = fork();
-    if (pid < 0) {
-      exit(1);
-    }
-    // Child process.
-    else if (pid == 0) {
-      if (execvp(args[0], args) < 0) {
+  }
+  // Implement FIFO pipe.
+  else {
+    int j = find_pipe(args);
+    if (j > -1) {
+      // printf("Found a pipe at %d!\n", j);
+      args[j] = NULL;
+      int BUFFER_SIZE = 256;
+      char buffer[BUFFER_SIZE];
+      char *abs_path = realpath(fifo_path, NULL);
+
+      int pid_in, pid_out, fd[2];
+      pipe(fd);
+      // Forking a child process to handle pipe_in.
+      pid_in = fork();
+      if (pid_in < 0) {
         exit(1);
+      } else if (pid_in == 0) {  // Child process (pipe_in).
+        // Forking a granchild process to handle pipe_out.
+        pid_out = fork();
+        if (pid_out < 0) {
+          exit(1);
+        } else if (pid_out == 0) {  // Grandchild process (pipe_out).
+          fd = open(abs_path, O_RDONLY);
+          read(fd, buffer, sizeof(buffer));
+          printf("In the buffer: %s\n", buffer);
+          close(fd);
+        } else {
+          fd = open(abs_path, O_WRONLY);
+          printf("Enter sth: ");
+          fgets(buffer, BUFFER_SIZE, stdin);
+          write(fd, buffer, strlen(buffer));
+          close(fd);
+        }
+      } else {  // Parent process.
+        close(fd[1]);
+        wait(NULL);
       }
     }
-    // Parent process.
+    // General case.
     else {
-      waitpid(-1, NULL, 0);  // Wait for any child.
+      pid_t pid = fork();
+      if (pid < 0) {
+        exit(1);
+      } else if (pid == 0) {  // Child process.
+        if (execvp(args[0], args) < 0) {
+          exit(1);
+        }
+      } else {                 // Parent process.
+        waitpid(-1, NULL, 0);  // Wait for any child.
+      }
     }
   }
 }
--- fifo_example.c
+++ fifo_example.c
@@ -6,5 +6,28 @@
 
 int main() {
   char *fifo_path =
-      '/Users/fnx/Dropbox/Academics/COMP 310/comp310-operating-system/fifo_example';
+      "/Users/fnx/Dropbox/Academics/COMP "
+      "310/comp310-operating-system/fifo_example_fifo";
+  mkfifo(fifo_path, 0777);
+
+  int pid = fork();
+  int BUFFER_SIZE = 256;
+  int fd;
+  char buffer[BUFFER_SIZE];
+
+  if (pid == 0) {
+    fd = open(fifo_path, O_WRONLY);
+    printf("Enter something: ");
+    fgets(buffer, BUFFER_SIZE, stdin);
+    write(fd, buffer, strlen(buffer));
+    close(fd);
+    printf("Done!\n");
+  } else {
+    fd = open(fifo_path, O_RDONLY);
+    read(fd, buffer, sizeof(buffer));
+    printf("You have typed: %s\n", buffer);
+    close(fd);
+  }
+
+  return 0;
 }
