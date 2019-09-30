6931db4ef59b3fb8d6a8b3a6a65d7adab69832d6
fangnx <fangnaxin@gmail.com>
2019-09-30 13:41:20 -0400

    Finished pipe and signal handling

--- assignment-1/tshell.c
+++ assignment-1/tshell.c
@@ -4,10 +4,11 @@
  * @author Naxin Fang
  * @github https://github.com/fangnx
  * @created 2019-09-21 15:42:16
- * @last-modified 2019-09-29 22:51:03
+ * @last-modified 2019-09-30 13:31:37
  */
 
 #include <fcntl.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -23,7 +24,7 @@
 
 #define PROMPT "\e[96m(naxin.fang)\e[0m \e[92m->\e[0m "
 #define MAX_LINE_LENGTH 120
-#define MAX_HISTORY_AMOUNT 10
+#define MAX_HISTORY_AMOUNT 100
 #define FIFO_PATH "/tmp/tshell_fifo"
 
 struct hist {
@@ -53,8 +54,6 @@ void parse_line(char line[], int length, char *commands[]) {
   token = strtok(line, delim);
   i = 0;
   while (token != NULL) {
-    // printf("%s\n", token);
-    // fflush(stdout);
     commands[i] = token;
     token = strtok(NULL, delim);
     i++;
@@ -98,10 +97,20 @@ void set_limit(char *args[]) {
   new_lim.rlim_max = old_lim.rlim_max;
 
   if (setrlimit(RLIMIT_NOFILE, &new_lim) == -1) {
-    printf("Error occurred during setting RLIMIT_DATA.");
+    printf("Error: an error occurred during setting RLIMIT.");
   }
   printf("New limits have been set.\nSoft limit: %llu\nHard limit: %llu\n",
          new_lim.rlim_cur, new_lim.rlim_max);
+}
+
+/**
+ * Execute a shell command. Display error message if the command is invalid.
+ */
+void execute_command(char *args[]) {
+  if (execvp(args[0], args) < 0) {
+    printf("tshell: command not found: %s\n", args[0]);
+    exit(1);
+  }
 }
 
 /**
@@ -129,7 +138,8 @@ void run_pipe(char *args[], int pipe_index) {
   mkfifo(FIFO_PATH, 0777);
   char *abs_path = realpath(FIFO_PATH, NULL);
   args[pipe_index] = NULL;
-  int pid_in, pid_out, fd[2], fdp;
+  pid_t pid_in, pid_out;
+  int fd[2], fdp;
   pipe(fd);
   // Forking a child process to handle pipe_in.
   pid_in = fork();
@@ -146,19 +156,22 @@ void run_pipe(char *args[], int pipe_index) {
       dup2(fdp, fileno(stdin));  // Redirect stdin to pipe.
       close(fd[1]);
       if (execvp(args[pipe_index + 1], &args[pipe_index + 1]) < 0) {
+        printf("tshell: command not found: %s\n", args[0]);
         exit(1);
-      };
+      }
     } else {
       // Implement pipe_in.
       fdp = open(abs_path, O_WRONLY);
       dup2(fdp, fileno(stdout));  // Redirect stdout to pipe.
       close(fd[0]);
       if (execvp(args[0], args) < 0) {
+        printf("tshell: command not found: %s\n", args[0]);
         exit(1);
       }
+      waitpid(pid_out, NULL, 0);
     }
   } else {  // Parent process.
-    waitpid(-1, NULL, 0);
+    waitpid(pid_in, NULL, 0);
   }
 }
 
@@ -185,9 +198,7 @@ void shell_system(char *args[]) {
       if (pid < 0) {
         exit(1);
       } else if (pid == 0) {  // Child process.
-        if (execvp(args[0], args) < 0) {
-          exit(1);
-        }
+        execute_command(args);
       } else {                 // Parent process.
         waitpid(-1, NULL, 0);  // Wait for any child.
       }
@@ -195,10 +206,28 @@ void shell_system(char *args[]) {
   }
 }
 
+// Handle SIGINT signal.
+void handle_sigint(int sig) {
+  char line[MAX_LINE_LENGTH];
+  printf("\nDo you want to exit tshell (y/n)?\n");
+  fgets(line, sizeof(line), stdin);
+  if (line[0] == 'y') {
+    exit(0);
+  }
+  printf(PROMPT);
+  fflush(stdout);
+}
+
+// Handle STGTSTP signal.
+void handle_sigtstp(int sig) { return; }
+
 int main(int argc, char *argv[]) {
   int line_length;
   char line[MAX_LINE_LENGTH];
   char *args[MAX_LINE_LENGTH];
+
+  signal(SIGINT, handle_sigint);
+  signal(SIGTSTP, handle_sigtstp);
 
   while (1) {
     memset(line, 0, MAX_LINE_LENGTH);
