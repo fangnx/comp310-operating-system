0813dc9366fbef86bf7d99ac8ae904535280973b
fangnx <fangnaxin@gmail.com>
2019-09-29 22:40:20 -0400

    A1 almost done

--- assignment-1/tiny_shell.c
+++ assignment-1/tiny_shell.c
@@ -1,10 +1,10 @@
 /**
- * COMP 310 / ECSE 427 (Fall 2019) - Assignment 1
+ * COMP 310 (Fall 2019) - Assignment 1
  *
  * @author Naxin Fang
  * @github https://github.com/fangnx
  * @created 2019-09-21 15:42:16
- * @last-modified 2019-09-29 18:36:49
+ * @last-modified 2019-09-29 22:32:24
  */
 
 #include <fcntl.h>
@@ -21,7 +21,7 @@
 #define min(a, b) ((a) < (b) ? (a) : (b))
 #endif
 
-#define PROMPT "(naxin.fang) -> "
+#define PROMPT "\e[96m(naxin.fang)\e[0m \e[92m->\e[0m "
 #define MAX_LINE_LENGTH 120
 #define MAX_HISTORY_AMOUNT 10
 
@@ -32,13 +32,16 @@ struct hist HISTORY[MAX_HISTORY_AMOUNT];
 int HIST_COUNT = 0;
 
 /**
+ * Read the raw stdin by the user.
  */
-int get_a_line(char line[]) { return read(0, line, MAX_LINE_LENGTH); }
+int read_line(char line[]) { return read(0, line, MAX_LINE_LENGTH); }
 
 /**
- * Tokenize the raw input, delimited by space.
+ * Tokenize the input line by splitting it by space.
+ *
+ * E.g, "ls -l" -> {"ls", "-l", NULL}.
  */
-void parse_commands(char line[], int length, char *commands[]) {
+void parse_line(char line[], int length, char *commands[]) {
   const char delim[] = " \t\n";
   char *token;
   int i;
@@ -57,12 +60,59 @@ void parse_commands(char line[], int length, char *commands[]) {
   }
 }
 
+/**
+ * Record the input line to history.
+ */
+void record_history(char args[]) {
+  strcpy(HISTORY[HIST_COUNT % MAX_HISTORY_AMOUNT].line, args);
+  HIST_COUNT++;
+}
+
+/**
+ * Implement "chdir" internal command.
+ */
+void change_dir(char *args[]) { chdir(args[1]); }
+
+/**
+ * Implement "history" internal command.
+ */
+void print_history() {
+  int i =
+      HIST_COUNT <= MAX_HISTORY_AMOUNT ? 0 : HIST_COUNT % MAX_HISTORY_AMOUNT;
+  int end_i = min(HIST_COUNT, i + MAX_HISTORY_AMOUNT);
+  while (i < end_i) {
+    printf("%3d %s\n", i + 1, HISTORY[i % MAX_HISTORY_AMOUNT].line);
+    i++;
+  }
+}
+
+/**
+ * Implement "limit" internal command.
+ */
+void set_limit(char *args[]) {
+  struct rlimit old_lim, new_lim;
+  getrlimit(RLIMIT_DATA, &old_lim);
+
+  new_lim.rlim_cur = atoi(args[1]);
+  new_lim.rlim_max = old_lim.rlim_max;
+
+  if (setrlimit(RLIMIT_NOFILE, &new_lim) == -1) {
+    printf("Error occurred during setting RLIMIT_DATA.");
+  }
+  printf("New limits have been set.\nSoft limit: %llu\nHard limit: %llu\n",
+         new_lim.rlim_cur, new_lim.rlim_max);
+}
+
+/**
+ * Auxiliary function to check if the tokenized input line contains the pipe
+ * character '|'.
+ *
+ * If true, return the index of '|'. Else, return -1.
+ */
 int find_pipe(char *args[]) {
   int i;
   i = 0;
   while (args[i] != NULL) {
-    // printf("%s\n", args[i]);
-    // fflush(stdout);
     if (strcmp(args[i], "|") == 0) {
       return i;
     }
@@ -72,86 +122,66 @@ int find_pipe(char *args[]) {
 }
 
 /**
- *
+ * Implemented FIFO pipe.
  */
-void my_system(char line[], char *args[]) {
+void run_pipe(char *args[], int pipe_index) {
   char *fifo_path =
       "/Users/fnx/Dropbox/Academics/COMP "
       "310/comp310-operating-system/assignment-1/fifo";
   mkfifo(fifo_path, 0777);
 
-  // Update history.
-  strcpy(HISTORY[HIST_COUNT % MAX_HISTORY_AMOUNT].line, line);
-  HIST_COUNT++;
-  // Implement "chdir" internal command.
-  if (strcmp(args[0], "chdir") == 0 || strcmp(args[0], "cd") == 0) {
-    chdir(args[1]);
-  }
-  // Implement "history" internal command.
-  else if (strcmp(args[0], "history") == 0) {
-    int i =
-        HIST_COUNT <= MAX_HISTORY_AMOUNT ? 0 : HIST_COUNT % MAX_HISTORY_AMOUNT;
-    int end_i = min(HIST_COUNT, i + MAX_HISTORY_AMOUNT);
-    while (i < end_i) {
-      printf("%3d %s\n", i + 1, HISTORY[i % MAX_HISTORY_AMOUNT].line);
-      i++;
-    }
-  }
-  // Implement "limit" internal command.
-  else if (strcmp(args[0], "limit") == 0) {
-    struct rlimit old_lim, new_lim;
-    getrlimit(RLIMIT_DATA, &old_lim);
+  args[pipe_index] = NULL;
+  char *abs_path = realpath(fifo_path, NULL);
 
-    new_lim.rlim_cur = atoi(args[1]);
-    new_lim.rlim_max = old_lim.rlim_max;
-
-    if (setrlimit(RLIMIT_NOFILE, &new_lim) == -1) {
-      printf("Error occurred during setting RLIMIT_DATA.");
-    }
-    printf("New limits have been set:\nsoft limit: %llu\nhard limit: %llu\n",
-           new_lim.rlim_cur, new_lim.rlim_max);
-  }
-  //
-  else if (strcmp(args[0], "exit") == 0) {
-    exit(0);
-  }
-  // Implement FIFO pipe.
-  else {
-    int j = find_pipe(args);
-    if (j > -1) {
-      // printf("Found a pipe at %d!\n", j);
-      args[j] = NULL;
-      int BUFFER_SIZE = 256;
-      char buffer[BUFFER_SIZE];
-      char *abs_path = realpath(fifo_path, NULL);
-
-      int pid_in, pid_out, fd[2];
-      pipe(fd);
-      // Forking a child process to handle pipe_in.
-      pid_in = fork();
-      if (pid_in < 0) {
+  int pid_in, pid_out, fd[2], fdp;
+  pipe(fd);
+  // Forking a child process to handle pipe_in.
+  pid_in = fork();
+  if (pid_in < 0) {
+    exit(1);
+  } else if (pid_in == 0) {  // Child process.
+    // Forking a granchild process to handle pipe_out.
+    pid_out = fork();
+    if (pid_out < 0) {
+      exit(1);
+    } else if (pid_out == 0) {  // Grandchild process.
+      // Implement pipe_out.
+      fdp = open(abs_path, O_RDONLY);
+      dup2(fdp, fileno(stdin));  // Redirect stdin to pipe.
+      close(fd[1]);
+      if (execvp(args[pipe_index + 1], &args[pipe_index + 1]) < 0) {
         exit(1);
-      } else if (pid_in == 0) {  // Child process (pipe_in).
-        // Forking a granchild process to handle pipe_out.
-        pid_out = fork();
-        if (pid_out < 0) {
-          exit(1);
-        } else if (pid_out == 0) {  // Grandchild process (pipe_out).
-          fd = open(abs_path, O_RDONLY);
-          read(fd, buffer, sizeof(buffer));
-          printf("In the buffer: %s\n", buffer);
-          close(fd);
-        } else {
-          fd = open(abs_path, O_WRONLY);
-          printf("Enter sth: ");
-          fgets(buffer, BUFFER_SIZE, stdin);
-          write(fd, buffer, strlen(buffer));
-          close(fd);
-        }
-      } else {  // Parent process.
-        close(fd[1]);
-        wait(NULL);
+      };
+    } else {
+      // Implement pipe_in.
+      fdp = open(abs_path, O_WRONLY);
+      dup2(fdp, fileno(stdout));  // Redirect stdout to pipe.
+      close(fd[0]);
+      if (execvp(args[0], args) < 0) {
+        exit(1);
       }
+    }
+  } else {  // Parent process.
+    wait(NULL);
+  }
+}
+
+/**
+ * Run commands in the t_shell.
+ */
+void shell_system(char *args[]) {
+  if (strcmp(args[0], "chdir") == 0 || strcmp(args[0], "cd") == 0) {
+    change_dir(args);
+  } else if (strcmp(args[0], "history") == 0) {
+    print_history();
+  } else if (strcmp(args[0], "limit") == 0) {
+    set_limit(args);
+  } else if (strcmp(args[0], "exit") == 0) {
+    exit(0);
+  } else {
+    int pipe_index = find_pipe(args);
+    if (pipe_index > -1) {
+      run_pipe(args, pipe_index);
     }
     // General case.
     else {
@@ -170,23 +200,24 @@ void my_system(char line[], char *args[]) {
 }
 
 int main(int argc, char *argv[]) {
-  int length;
+  int line_length;
   char line[MAX_LINE_LENGTH];
-  char *commands[MAX_LINE_LENGTH];
+  char *args[MAX_LINE_LENGTH];
 
   while (1) {
     memset(line, 0, MAX_LINE_LENGTH);
-    memset(commands, 0, MAX_LINE_LENGTH);
+    memset(args, 0, MAX_LINE_LENGTH);
 
     printf(PROMPT);  // Print shell line prompt.
     fflush(stdout);
 
-    length = get_a_line(line);
+    line_length = read_line(line);
+    if (line_length > 1) {
+      parse_line(line, line_length, args);
+      record_history(*args);
 
-    if (length > 1) {
-      parse_commands(line, length, commands);
       fflush(stdout);
-      my_system(line, commands);
+      shell_system(args);
     }
   }
   return 0;
